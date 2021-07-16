4abec768d7ac513ab4f65ac65557f6a12de1b00b
nolgg <nol37845@gmail.com>
2021-07-16 09:28:10 +0700

    uptodatenaja

--- CS01-6.py
+++ CS01-6.py
@@ -1,3 +1,5 @@
 A = int(input('A :'))
-if(A < 10):
-    print('yes')
+B = int(input('B :'))
+C = int(input('C :'))
+if(A < 10 or B > 5 and C > 7):
+    print('yes') 
---
+++ CS01-7.py
@@ -0,0 +1,7 @@
+A = int(input('A :'))
+if (A <= 10):
+    print('ไม่ผ่าน')
+elif (A <= 20):
+    print('ปรับปรุง')
+elif (A <= 30):
+    print('ดีมาก')        
